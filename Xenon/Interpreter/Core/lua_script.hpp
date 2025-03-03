6cbd3c890e9cd4a9df7cc34c1045cb3c30961c74
zemmezzer <drstive121319@gmail.com>
2025-03-03 19:18:23 +0100

    init commit

---
+++ Xenon/Interpreter/Core/lua_script.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include "Lua.hpp"
+
+#include "Lua/lauxlib.h"
+#include "Lua/lua.h"
+
+enum class LuaScriptType
+{
+  PlainText,
+  Compiled,
+  Compressed,
+  CompiledAndCompressed,
+};
+
+struct LuaScript
+{
+  const char *path;
+  const char *content;
+  const unsigned int length;
+  const LuaScriptType type;
+
+  int call(lua_State *L) const
+  {
+#ifdef LUA_USE_COMPRESSION
+    // TODO: implement gzip compression
+#endif
+
+    int status = luaL_loadbuffer(L, content, length, path);
+    if (status != LUA_OK)
+    {
+      return status;
+    }
+
+    status = lua_pcall(L, 0, LUA_MULTRET, 0);
+    if (status != LUA_OK)
+    {
+      return status;
+    }
+
+    return LUA_OK;
+  }
+};
---
+++ Xenon/Interpreter/Interpreter.cpp
@@ -0,0 +1,9 @@
+#include "Core/Lua.hpp"
+#include "Core/LuaContext.hpp"
+#include "Core/Registry/LuaCodeSnippet.hpp"
+
+
+void load_script()
+{
+    
+}
